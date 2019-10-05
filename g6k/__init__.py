fba8de925860caf793c1aa7f351738131f624aa4
J08nY <johny@neuromancer.sk>
2019-10-05 13:30:04 +0200

    Python3: Fix siever import.

--- g6k/__init__.py
+++ g6k/__init__.py
@@ -3,7 +3,3 @@
 from __future__ import absolute_import
 from .siever_params import SieverParams # noqa
 from .siever import Siever # noqa
-
-# NOTE for compatibility with older pickles
-import siever # noqa
-siever.SieverParams = SieverParams # noqa
