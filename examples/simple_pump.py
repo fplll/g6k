4ddd054682e43044b1b85e9cb447bcbb0cebb1b1
Leo Ducas <ducas@cwi.nl>
2020-05-07 10:48:22 +0200

    tabs -> spaces

--- examples/simple_pump.py
+++ examples/simple_pump.py
@@ -2,19 +2,19 @@
 
 # A simple insertion policy: choose the leftmost candidate, no further than aux.
 def scoring(i, nlen, olen, aux):
-	return -i if i >= aux else 9999
+    return -i if i >= aux else 9999
 
 # A simple implementation of the pump, with minimal amount of parameters.
 # Significant loss of performances to be expected compared to the one from
 # ../g6k/algorithms/pump.p
 
 def pump(g6k, kappa, blocksize, dim4free):
-	# The right end of the sieve context (constant through the pump)
-	r = kappa+blocksize
-	# The left end of the sieve context (at the top of the pump)
-	l = kappa+dim4free
+    # The right end of the sieve context (constant through the pump)
+    r = kappa+blocksize
+    # The left end of the sieve context (at the top of the pump)
+    l = kappa+dim4free
 
-	# clear the database
+    # clear the database
     g6k.shrink_db(0)
     # make sure the current block is reasonably reduced
     g6k.lll(kappa, r)
@@ -24,19 +24,19 @@ def pump(g6k, kappa, blocksize, dim4free):
 
     # Pump up
     while g6k.l > l:
-    	g6k.extend_left(1)
-    	g6k.sieve()
+        g6k.extend_left(1)
+        g6k.sieve()
 
     # pump down
     aux = kappa
     while g6k.l < r+20:
-    	# Insert the leftmost candidate if we have one
-    	ii = g6k.insert_best_lift(scoring, aux)
-    	# If no insertion happened
-    	if ii is None:
-    		# Force decrease of the sieve context
-    		g6k.shrink_left(1)
-    	else:
-    		# Forbid future insertion at before the inserted position 
-    		# (we don't want to break previously done work)
-    		aux = ii+1
+        # Insert the leftmost candidate if we have one
+        ii = g6k.insert_best_lift(scoring, aux)
+        # If no insertion happened
+        if ii is None:
+            # Force decrease of the sieve context
+            g6k.shrink_left(1)
+        else:
+            # Forbid future insertion at before the inserted position 
+            # (we don't want to break previously done work)
+            aux = ii+1
