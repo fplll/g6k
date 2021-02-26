744550420c1b5f0602a8fe56d05efcce72bd3609
cr-marcstevens <github@marc-stevens.nl>
2021-02-26 19:54:10 +0100

    Add LICENSE and copyright statements to source files

---
+++ g6k/utils/__init__.py
@@ -0,0 +1,20 @@
+####
+#
+#   Copyright (C) 2018-2021 Team G6K
+#
+#   This file is part of G6K. G6K is free software:
+#   you can redistribute it and/or modify it under the terms of the
+#   GNU General Public License as published by the Free Software Foundation,
+#   either version 2 of the License, or (at your option) any later version.
+#
+#   G6K is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with G6K. If not, see <http://www.gnu.org/licenses/>.
+#
+####
+
+
