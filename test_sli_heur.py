af12d5afe98e5bdde6da344d99aae9ac6483bc39
alexgit256 <tremeloesalex@gmail.com>
2024-08-09 16:54:38 +0400

    Prediction for rerand num added

--- test_sli_heur.py
+++ test_sli_heur.py
@@ -16,7 +16,7 @@ from fpylll.util import gaussian_heuristic
 FPLLL.set_random_seed(0x1337)
 from g6k.siever import Siever
 from g6k.siever_params import SieverParams
-from math import sqrt, ceil, floor
+from math import sqrt, ceil, floor, log, exp
 from copy import deepcopy
 from random import shuffle, randrange
 
@@ -199,11 +199,63 @@ def run_exp(g6k_obj, approx_fact=0.5, n_targets=1):
     print( exp_results )
     return exp_results
 
+def batchCVPP_prob(d, alpha, gamma):
+    # as per https://wesselvanwoerden.com/publication/ducas-2020-randomized/randomized_slicer.pdf
+    #https://eprint.iacr.org/2016/888.pdf gives the same numbers since we have sqrt(4/3) short vectors given
+    #alpha = sqrt(4/3.) #1.00995049383621 #<2*c - 2*sqrt(c^2-c) < 1.10468547559928 since a <1.22033 (see p.22)
+    #gamma = 1     #gamma-CVP
+    a = alpha**2
+    b = a**2/(4*a - 4)
+    c = gamma**2
+    assert (b>c), f"Bad b: {b} <= {c}"
+    n = ceil(-1/2 + sqrt((4*b-a)**2-8*c*(2*b-a))/(2*a))
+
+    #print(a, b, c, n)
+
+    #Eq.(12)
+    def p(a, x, y):
+        return sqrt(a - (a+x-y)**2/(4*x))
+
+    #Eq.(16)
+    def omega(a, x, y):
+        return -log(p(a,x,y))
+
+    if n==1:
+        u = 0
+        v = c-b
+
+    else:
+        disc = (a*n**2 - (b+c))**2 + 4*b*c*(n**2-1)
+        u = ( (b+c-a)*n - sqrt(disc) )/(n**3 - n)
+        v = ( (a-2*b)*n**2 + (b-c) + n*sqrt(disc) )/(n**3-n)
+
+
+    # Eq.(43) success probablity for one target
+    prob = 0
+    x_ = [0]*(n+1)
+    x_[0] = b
+    x_[n] = c
+    for i in range(1, n+1):
+        x_[i] = u*i**2+v*i+b
+        prob += omega(a, x_[i-1], x_[i])
+        #print(i, x_[i].n(), prob)
+
+    # T = (a - 2*(a-1)/(1+sqrt(1-1./a)))**(-1/2.)  #base for power-d, runtime per instance!
+
+    prob = exp(-prob) #1/prob=number of rerandomizations per target, base for power-d
+
+    # T = d*log(1./prob*T,2) + M
+    #assert(M<d*(log(alpha,2)+log(1./prob,2))), f"!"
+
+    #print("prob:", prob)  # 0.901387818865997 for a = 4/3
+    #print("T:", T)        # 1.06066017177982 for a = 4/3
+    #print("RT:", 1./prob*T, log(1./prob*T, 2).n()) #1.17669681082910 for a = 4/3
+    return prob
 
 if __name__ == "__main__":
     n_workers = 2
     n_lats = 5
-    n_instances_per_lattice = 100
+    n_instances_per_lattice = 10
     n, k, bits, betamax = 70, 35, 13.8, 40
 
     # - - - Generate lattices - - -
@@ -242,8 +294,10 @@ if __name__ == "__main__":
         sli_time_list += [ tt[2] for tt in stats]
 
         print(f"n_rerand_sli_list:")
-        print(f"    avg: {np.average(n_rerand_sli_list)}, med: {np.median(n_rerand_sli_list)}, min: {np.min(n_rerand_sli_list)}, max: {np.max(n_rerand_sli_list)} std: {np.std(n_rerand_sli_list)}")
+        print(f"    avg: {np.average(n_rerand_sli_list)}, med: {np.median(n_rerand_sli_list)}, min: {np.min(n_rerand_sli_list)}, max: {np.max(n_rerand_sli_list)}, std: {np.std(n_rerand_sli_list)}")
         print()
         print(f"sli_time_list:")
-        print(f"    avg: {np.average(sli_time_list)}, med: {np.median(sli_time_list)}, std: {np.std(sli_time_list)}")
+        print(f"    avg: {np.average(sli_time_list)}, med: {np.median(sli_time_list)}, max: {np.max(sli_time_list)}, std: {np.std(sli_time_list)}")
+        print()
+        print(f"vs predicted: {batchCVPP_prob(n, sqrt(4./3.), 1.)**(-n/2.)}")
         print()
